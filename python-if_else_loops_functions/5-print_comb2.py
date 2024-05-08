8836d2ecc3a78eb3c069e7cef2fc96e9b5af672c
OscarRapale <you@example.com>
2024-05-08 15:12:43 -0400

    Correcting format

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-
-print(', '.join(f"{i:02}" for i in range(0, 100)))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
