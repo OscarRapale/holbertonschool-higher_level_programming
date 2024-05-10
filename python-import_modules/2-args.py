020f5647e2081e9f550ddf53dc8ffc66b1dcb7b4
OscarRapale <you@example.com>
2024-05-10 14:07:22 -0400

    Task 2 added

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
+
+    import sys
 
     num_args = len(sys.argv) - 1
 
@@ -14,5 +15,5 @@ if __name__ == "__main__":
     else:
         print(f"{num_args} arguments:")
 
-    for i in range(1, len(sys.argv)):
-        print(sys.argv[i])
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print(f"{i}: {arg}")
