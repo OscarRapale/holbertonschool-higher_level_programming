1e7c3d9dee98eeaa3d200e5ee153121dfdaacebd
OscarRapale <you@example.com>
2024-05-14 16:08:14 -0400

    Task 2 added

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+
+    unique_int = set()
+
+    for element in my_list:
+        unique_int.add(element)
+        result = sum(unique_int)
+
+    return result
