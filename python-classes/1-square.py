859734d13b000fafae3c7da3541c91c346c75915
OscarRapale <you@example.com>
2024-05-17 15:03:08 -0400

    Task 1 added

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define a empty class square."""
+
+
+class Square:
+
+    def __init__(self, size):
+
+        """Initialize squatre
+        
+        Args:
+        size: size pf square"""
+
+        self.__size = size
