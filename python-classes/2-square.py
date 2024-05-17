9cc1c6aa38c695747abff50f39d91c578a08d001
OscarRapale <you@example.com>
2024-05-17 15:32:10 -0400

    Task 2 added

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Define a class square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+
+        """Initialize square.
+
+        Args:
+            size: size of square.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
