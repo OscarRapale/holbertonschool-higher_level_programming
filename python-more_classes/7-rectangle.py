883bf8c8d3548d4a28d87679de9f7980a382cbed
OscarRapale <you@example.com>
2024-05-21 18:26:35 -0400

    Task 7 added

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -64,9 +64,12 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
 
-        row = str(Rectangle.print_symbol) * self.__width
-        rectangle = (row + "\n") * self.__height
-        return rectangle.rstrip('\n')
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
