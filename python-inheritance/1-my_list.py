c4011874776b8df144170b36ba2756602d91c29b
OscarRapale <you@example.com>
2024-05-22 15:24:42 -0400

    Task 1 added

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -5,7 +5,7 @@ contains the MyList class
 
 
 class MyList(list):
-    """A subclass of list with a method to print the list sorted in ascending order."""
+    """A subclass of list to print the list in ascending order."""
 
     def print_sorted(self):
         """Prints the list sorted in ascending order."""
