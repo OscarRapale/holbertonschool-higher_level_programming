452db2a4b43c0a2d39412201c4e709873d9e5608
OscarRapale <you@example.com>
2024-05-30 16:01:42 -0400

    task 03 added

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -10,22 +10,30 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         Handles GET requests. Responds with JSON data for '/data' endpoint, 'OK' for '/status' endpoint,
         and 'Not Found' for undefined endpoints.
         """
-        if self.path == '/data':
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Hello, this is a simple API!')
+
+        elif self.path == '/data':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode())
+
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'OK')
+
         else:
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b'Not Found')
+            self.wfile.write(b'Endpoint not defined')
 
 def run(server_class=http.server.HTTPServer, handler_class=MyHTTPRequestHandler):
     """
