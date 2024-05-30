56d99d8aaf873cac2236025ca7962e86b34bc76b
OscarRapale <you@example.com>
2024-05-30 18:29:30 -0400

    Fixing undefined endpoint msg

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -37,7 +37,7 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(info).encode())
 
         else:
-            self.send_response(404, "Not Found")
+            self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b'Endpoint not found')
