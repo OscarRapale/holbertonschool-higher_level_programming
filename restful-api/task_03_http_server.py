6f0cfab7caa2d2c9c5659358aad81ef55cf18e46
OscarRapale <you@example.com>
2024-05-30 16:29:32 -0400

    fixing endpoint message

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,14 +1,16 @@
 import http.server
 import json
+import logging
 
 class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     """
-    HTTP request handler class that handles GET requests for '/data', '/status', and undefined endpoints.
+    HTTP request handler class that handles GET requests for '/' ,'/data', '/status', '/info' and undefined endpoints.
     """
+
     def do_GET(self):
         """
-        Handles GET requests. Responds with JSON data for '/data' endpoint, 'OK' for '/status' endpoint,
-        and 'Not Found' for undefined endpoints.
+        Handles GET requests. Responds with JSON data for '/data' and '/info' endpoints,
+        'OK' for '/status' endpoint, and 'Not Found' for undefined endpoints.
         """
         if self.path == '/':
             self.send_response(200)
@@ -46,8 +48,16 @@ def run(server_class=http.server.HTTPServer, handler_class=MyHTTPRequestHandler)
     """
     Starts the HTTP server with the specified server class and request handler class.
     """
+    logging.basicConfig(level=logging.INFO)
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+    logging.info("Starting server on port 8000...")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info("Stopping server.")
 
-run()
+if __name__ == '__main__':
+    run()
