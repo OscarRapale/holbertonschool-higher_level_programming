60e73d86702eb1bd713c9e319c8277e966be9568
OscarRapale <you@example.com>
2024-06-17 21:05:12 -0400

    Task 9 added

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Create a full table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(256,
-    score INT)
+    name VARCHAR(256),
+    score INT
 );
 INSERT INTO second_table (id, name, score)
 VALUES
