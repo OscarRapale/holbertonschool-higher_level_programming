ea356ca5bd2406dce04263032c73fe43f2b65ff3
OscarRapale <you@example.com>
2024-06-19 18:19:53 -0400

    task 0 added

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """script that lists all states from the database"""
 
+
 import MySQLdb
+
 from sys import argv
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     username = argv[1]
     password = argv[2]
     database_name = argv[3]
@@ -12,10 +15,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=database_name)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
