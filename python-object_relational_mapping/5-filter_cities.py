1891386997ff4eb3542a8585eb8fa97bd22336b7
OscarRapale <you@example.com>
2024-06-20 17:12:32 -0400

    Task 5 added

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
-    
+
     cur = db.cursor()
     cur.execute("""SELECT cities.name FROM cities
                JOIN states ON states.id=cities.state_id
@@ -18,5 +18,3 @@ if __name__ == "__main__":
     print(*cities, sep=", ")
     cur.close()
     db.close()
-
-
